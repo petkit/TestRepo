7435c82ea7693924e6993c4939937dc584dd720e
petkit <pk_283@hotmail.com>
2014-08-29 18:19:00 +0200

    Testing MVC Razor views

---
+++ TestProject/TestWeb/Models/TestItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestWeb.Models
+{
+	public class TestItem
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
